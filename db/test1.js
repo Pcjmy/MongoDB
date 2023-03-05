13915810ef98fda6e4379ce9d4b3056698034382
Pcjmy <1716045191@qq.com>
2023-03-05 16:04:53 +0800

    feat: 使用model操作数据库

--- db/test1.js
+++ db/test1.js
@@ -1,5 +1,16 @@
-// 使用 model
+// 使用 model 操作数据库
 
 const { User } = require('./model')
 
-console.log('ok')
+// 定义一个async的匿名函数，并执行。为了里面能用await
+!(async () => {
+  // 新增数据
+  const zhangsan = new User({
+    username: 'zhangsan',
+    password: 'abc',
+    age: 20,
+    city: 'beijing',
+    gender: 1
+  })
+  zhangsan.save()
+})()
